41b25255f7a040a298b5a0e1b9b3de5261a1c7bd
Elie Chammas <contact@eliechammas.me>
2025-05-09 00:51:47 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//add application Insights Telemetry
+builder.Services.AddApplicationInsightsTelemetry();
+
+// ensure my application listens on port 8081
+builder.WebHost.UseUrls("https://0.0.0.0:8081");
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
